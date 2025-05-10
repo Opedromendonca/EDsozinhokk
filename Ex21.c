29965e48ccc709800c11e8a0d12ab61522e4f82e
Opedromendonca <pedro.202311336@unilasalle.edu.br>
2025-05-10 16:50:37 -0400

    inicio

---
+++ Ex21.c
@@ -0,0 +1,22 @@
+/*
+ * File: Ex21.c
+ * Author: 202311336,pedro mendonca,pedro.202311336@unilasalle.edu.br'	
+ * Created on: 2025-05-10
+ */
+ 
+// MOD-001 - INI - Pedro
+#include <stdio.h>
+
+int main() {
+    int ids[10];
+    ids[0] = 01;
+    
+    for (int i = 0; i < 10; i++) {
+	
+	printf("%d ", ids[i]);
+		printf("\n");
+	}
+	
+	return 0;
+}
+// MOD-001 - FIM - Pedro
