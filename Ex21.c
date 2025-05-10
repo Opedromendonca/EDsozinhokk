a8d331427fb14c0304a6d1bc33ad0a5170c3e3ff
Opedromendonca <pedro.202311336@unilasalle.edu.br>
2025-05-10 17:32:20 -0400

    v2

--- Ex21.c
+++ Ex21.c
@@ -6,16 +6,53 @@
  
 // MOD-001 - INI - Pedro
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+int ID;
+float g1,g2,media;
+} Aluno;
+
+typedef struct {
+Aluno *aluno;
+int top;
+} Stack;
+
 
 int main() {
-    int ids[10];
-    ids[0] = 01;
+    Stack stack;
+    int cap;
     
-    for (int i = 0; i < 10; i++) {
+    printf("capacidade: ");
+    scanf("%d", &cap);
 	
-	printf("%d ", ids[i]);
-		printf("\n");
-	}
+	stack.aluno = (Aluno *)malloc(cap * sizeof(Aluno));
+	stack.top = -1;
+	
+	int opt;
+	do {
+		
+		printf("1 - push \n2 - pop \n3 - find \n0 - exit ");
+		scanf("\n qual?%d", &opt);
+		
+		switch (opt) {
+			
+			case 1:
+			
+			
+			//push();
+			case 2:
+			//pop();
+			case 3:
+			int id;
+			printf("qual id? ");
+			scanf("%d", &id);
+			//find();
+			
+		}
+		
+		
+	} while(opt !=0);
 	
 	return 0;
 }
